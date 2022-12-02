8f859f65d2b1198dab10f6e8197484fc5acdd333
rushabhshah <rushabh795@gmail.com>
2022-12-02 13:49:06 -0500

    Added Project

---
+++ app/src/main/java/com/careerconnect/UploadCv.java
@@ -0,0 +1,21 @@
+package com.careerconnect;
+
+public class UploadCv {
+
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public UploadCv(String url ) {
+
+        this.url = url;
+    }
+
+
+    public String url;
+
+
+
+}
